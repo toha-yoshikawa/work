2e63a267f7219c5e79d238435793afdfd5820330
systena <systena@pck-137-008.local>
2018-05-14 23:02:14 +0900

    Initial Commit

---
+++ Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shopping
+//
+//  Created by systena on 2018/05/14.
+//  Copyright © 2018年 systena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

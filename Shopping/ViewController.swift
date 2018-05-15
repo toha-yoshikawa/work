d56edd57ea7b328ee50e5611a1a2178f8afa319f
systena <systena@PCK-137-008.local>
2018-05-16 02:09:36 +0900

    create new project

--- Shopping/ViewController.swift
+++ Shopping/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cell1: UITableViewCell!
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var cell2: UITableViewCell!
+    @IBOutlet weak var cell3: UITableViewCell!
+    @IBOutlet weak var cell4: UITableViewCell!
+    @IBOutlet weak var text1: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        cell1.text = '太鼓'
+//cell2.text = 'バチ'
+//cell3 = 'タンバリン'
+  //      cell4 = 'チャリンコ'
+        //text1.text = "チャリンコ"
     }
 
     override func didReceiveMemoryWarning() {
